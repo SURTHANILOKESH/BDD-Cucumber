e9d6151f472b1f9dc07b68401312c2daad3363d2
LOKESH <lokesh_surthani>
2022-03-24 17:08:56 +0530

    Adding files

---
+++ src/test/java/StepDefinitions/CartSteps.java
@@ -0,0 +1,27 @@
+package stepdefinitions;
+
+import org.testng.Assert;
+
+import PageObjects.HomePage;
+import PageObjects.LoginPage;
+import Utilities.TestContext;
+import io.cucumber.java.en.When;
+
+public class CartSteps {
+
+    TestContext testContext;
+    HomePage homePage;
+    LoginPage loginPage;
+
+    public CartSteps(TestContext context) {
+        testContext = context;
+        homePage = testContext.getPageObjectManager().getHomePage();
+        loginPage = testContext.getPageObjectManager().getLoginPage();
+    }
+
+    @When("Go to cart page without authorizations")
+    public void goToCartPageWithoutAuthorizations() {
+        homePage.clickCartButton();
+        Assert.assertTrue(loginPage.emailLoginPageIsDisplayed());
+    }
+}
